eda76e219010dec5ce125c0fc45d19334e496155
Brad <bradpf@outlook.com>
2018-03-01 01:12:13 -0700

    Working on chat styling and ChatMessage component

---
+++ src/view/ChatMessage.js
@@ -0,0 +1,22 @@
+import classNames from 'classnames'
+
+import { relativeStamp } from '../utils/time'
+import getAccessibleText from '../utils/getAccessibleText'
+
+function ChatMessage ({ msg, date, backgroundColor, tail = false }) {
+  const accessibleTxtColor = getAccessibleText(backgroundColor)
+  const accessibleTxtRBG = `rgb(${accessibleTxtColor.r}${accessibleTxtColor.g}${accessibleTxtColor.b})`
+  const backgroundRGB = `rgb(${backgroundColor.r}${backgroundColor.g}${backgroundColor.b})`
+
+  return (
+    `<div style={ background-color: ${backgroundRGB}; color: ${accessibleTxtRBG} } class="${classNames(
+      'msg-container outgoing',
+      { 'tail': tail }
+    )}">
+      <span class="msg">${msg}</span>
+      <span class="timestamp">${relativeStamp(date)}</span>
+    </div>`
+  )
+}
+
+export default ChatMessage
